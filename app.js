7a22dea12c5fcaf40cd9523846cd28c7f5f4fa0f
Usama <us273852@gmail.com>
2022-08-10 14:37:41 +0500

    test

--- app.js
+++ app.js
@@ -39,15 +39,9 @@ app.use((req,res,next)=>{
     })
 });
 
-app.use((req,res,next)=>{
-    res.status(404).json({
-        Error : 'url not found'
-    })
-});
-
 // app.use((req,res,next)=>{
 //     res.status(404).json({
-//         error : 'Bad request'
+//         Error : 'url not found'
 //     })
 // });
 
