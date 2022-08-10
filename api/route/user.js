e2464d704a915e7e7582decf699c0f956cb15da0
Usama <us273852@gmail.com>
2022-08-10 13:08:05 +0500

    Test commit

--- api/route/user.js
+++ api/route/user.js
@@ -89,25 +89,25 @@ router.post('/signin',(req,res,next)=>{
 // })
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'User route working'
-    })
-})
-
 // router.get('/', (req, res, next) => {
-//     User.find()
-//     .then(result=>{
-//         res.status(200).json({
-//             result : result
-//         });
-//     })
-//     .catch(err=>{
-//         res.status(500).json({
-//             error : err
-//         })
-//     });
+//     res.status(200).json({
+//         message: 'User route working'
+//     }) 
 // })
+
+router.get('/', (req, res, next) => {
+    User.find()
+    .then(result=>{
+        res.status(200).json({
+            result : result
+        });
+    })
+    .catch(err=>{
+        res.status(500).json({
+            error : err
+        })
+    });
+})
 
 
 module.exports = router;
